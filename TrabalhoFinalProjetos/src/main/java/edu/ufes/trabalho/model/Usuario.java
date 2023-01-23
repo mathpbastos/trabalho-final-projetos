98b26d8b02f24d7df4baa1c4df25ac73ada113dc
Matheus Bastos <mathpbastos@gmail.com>
2023-01-23 16:22:15 -0300

    fix: model usuario

--- TrabalhoFinalProjetos/src/main/java/edu/ufes/trabalho/model/Usuario.java
+++ TrabalhoFinalProjetos/src/main/java/edu/ufes/trabalho/model/Usuario.java
@@ -10,14 +10,17 @@ public class Usuario {
     private String senha;
     private LocalDate dataCadastro;
     private LocalDate dataModificacao;
-    
+    private boolean administrador;
+    private boolean autorizado;
     
     public Usuario(String nome, 
             String login, 
-            String senha) {
+            String senha,
+            boolean administrador) {
         this.nome = nome;
         this.login = login;
         this.senha = senha;
+        this.administrador = administrador;
     }
 
     public Usuario(long id, 
@@ -25,23 +28,42 @@ public class Usuario {
             String login, 
             String senha, 
             LocalDate dataCadastro,
-            LocalDate dataModificacao) {
+            LocalDate dataModificacao,
+            boolean administrador) {
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        this.dataCadastro = dataCadastro;
+        this.dataModificacao = dataModificacao;
+        this.administrador = administrador;
+    }
+    
+    public Usuario(long id, 
+            String nome, 
+            String login, 
+            String senha, 
+            LocalDate dataCadastro,
+            LocalDate dataModificacao,
+            boolean administrador,
+            boolean autorizado) {
         this.id = id;
         this.nome = nome;
         this.login = login;
         this.senha = senha;
         this.dataCadastro = dataCadastro;
         this.dataModificacao = dataModificacao;
+        this.administrador = administrador;
+        this.autorizado = autorizado;
     }
 
     public long getId() {
         return id;
     }
-
+    
     public String getNome() {
         return nome;
     }
-
+    
     public String getLogin() {
         return login;
     }
@@ -53,5 +75,44 @@ public class Usuario {
     public LocalDate getDataCadastro() {
         return dataCadastro;
     }
-        
+    
+    public boolean isAdministrador(){
+        return this.administrador;
+    }
+      
+    public boolean isAutorizado(){
+        return this.autorizado;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setAdministrador(boolean administrador) {
+        this.administrador = administrador;
+    }
+
+    public void setAutorizado(boolean autorizado) {
+        this.autorizado = autorizado;
+    }
+    
+    public String toString(){
+        return "Id: " + this.id
+                + "\nNome: " + this.nome
+                + "\nLogin: " + this.login
+                + "\nSenha: " + this.senha
+                + "\nData Cadastro: " + this.dataCadastro
+                + "\nData Mod: " + this.dataModificacao
+                + "\nAdministrador: " + this.administrador
+                + "\nAutorizado: " + this.autorizado;
+    }
+    
 }
