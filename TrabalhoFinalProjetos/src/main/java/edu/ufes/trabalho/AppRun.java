65b5b27fd10d5a2ece68ffe9ec2a3bcb7a037b00
Matheus Bastos <mathpbastos@gmail.com>
2023-01-26 12:10:56 -0300

    init tela principal

--- TrabalhoFinalProjetos/src/main/java/edu/ufes/trabalho/AppRun.java
+++ TrabalhoFinalProjetos/src/main/java/edu/ufes/trabalho/AppRun.java
@@ -1,8 +1,10 @@
 package edu.ufes.trabalho;
 
+import edu.ufes.trabalho.presenter.principal.TelaPrincipalPresenter;
+
 public class AppRun {
 
     public static void main(String[] args) {
-        
+        new TelaPrincipalPresenter();
     }
 }
